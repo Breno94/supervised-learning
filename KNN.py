79bf07dcb9b54c873bffb914e5c8051927eb066f
Santos <breno.i.santos@accenture.com>
2021-11-25 15:01:42 -0300

    Formated to function as a module.

--- KNN.py
+++ KNN.py
@@ -24,31 +24,32 @@ class KNN(object):
         return np.reshape(np.array(y), newshape=(-1, 1))
 
 
-cols = ['SEPAL_LENGTH', 'SEPAL_WIDTH', 'PETAL_LENGTH', 'PETAL_WIDTH', 'CLASS']
-df = pd.read_csv(r'https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv')
-df.columns = cols
-labels = {
-    'Iris-setosa': 0,
-    'Iris-versicolor': 1,
-    'Iris-virginica': 2
-}
-df['CLASS'] = df['CLASS'].map(labels)
-df = df.sample(frac=1, random_state=1234) # Shuffling the data
+if __name__ == '__main__':
+    cols = ['SEPAL_LENGTH', 'SEPAL_WIDTH', 'PETAL_LENGTH', 'PETAL_WIDTH', 'CLASS']
+    df = pd.read_csv(r'https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv')
+    df.columns = cols
+    labels = {
+        'Iris-setosa': 0,
+        'Iris-versicolor': 1,
+        'Iris-virginica': 2
+    }
+    df['CLASS'] = df['CLASS'].map(labels)
+    df = df.sample(frac=1, random_state=1234) # Shuffling the data
 
-def train_test_split(X, y, train_size=0.8):
-    size = int(X.shape[0]*train_size)
-    return X[:size], X[size:], y[:size], y[size:]
-
-
-X_train, X_test, y_train, y_test = train_test_split(df.drop(columns='CLASS').values, df.CLASS.values)
-y_train, y_test = np.reshape(y_train, newshape=(-1, 1)), np.reshape(y_test, newshape=(-1, 1))
-
-def accuracy_score(y_true, y_pred):
-    return y_pred[y_pred[:, 0] == y_true[:, 0]].shape[0] / y_true.shape[0]
+    def train_test_split(X, y, train_size=0.8):
+        size = int(X.shape[0]*train_size)
+        return X[:size], X[size:], y[:size], y[size:]
 
 
-for k in [3, 4, 5, 6, 7, 8, 9, 10]:
-    knn = KNN(k=k)
-    knn.fit(X_train, y_train)
-    y_pred = knn.predict(X_test)
-    print(f'K = {k} - {round(accuracy_score(y_test, y_pred), 4)}')
+    X_train, X_test, y_train, y_test = train_test_split(df.drop(columns='CLASS').values, df.CLASS.values)
+    y_train, y_test = np.reshape(y_train, newshape=(-1, 1)), np.reshape(y_test, newshape=(-1, 1))
+
+    def accuracy_score(y_true, y_pred):
+        return y_pred[y_pred[:, 0] == y_true[:, 0]].shape[0] / y_true.shape[0]
+
+
+    for k in [3, 4, 5, 6, 7, 8, 9, 10]:
+        knn = KNN(k=k)
+        knn.fit(X_train, y_train)
+        y_pred = knn.predict(X_test)
+        print(f'k = {k} - Accuracy: {round(accuracy_score(y_test, y_pred), 4)}')
